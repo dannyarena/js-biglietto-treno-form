fb7c759b6af6ff7ecc45aa7254b9376f9e160aa1
dannyarena <arenadanny71@gmail.com>
2025-04-10 16:26:41 +0200

    refactor ticket price calculation to use event listener for button click

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
-const eta = parseInt(prompt("Quanti anni hai?"));
-
-const prezzoBase = km * 0.21;
-
-let prezzoFinale
-
-if (eta < 18) {
-    prezzoFinale = prezzoBase * 0.8;
-  } else if (eta > 65) {
-    prezzoFinale = prezzoBase * 0.6;
-  } else {
-    prezzoFinale = prezzoBase;
-  }
-
-  prezzoFinale = prezzoFinale.toFixed(2);
-
-  console.log("Il prezzo del tuo biglietto è: " + prezzoFinale + "€");
+document.getElementById('calcolaBtn').addEventListener('click', function () {
+    
+    
+  });
+  
