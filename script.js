b3ecf8f516613c89493fac0d08ba473be0248d61
dannyarena <arenadanny71@gmail.com>
2025-04-10 16:30:01 +0200

    add ticket price calculation logic and integrate with input fields

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('calcolaBtn').addEventListener('click', function () {
-    
-    
+    const km = parseInt(document.getElementById('km').value);
+    const eta = parseInt(document.getElementById('eta').value);
   });
   
