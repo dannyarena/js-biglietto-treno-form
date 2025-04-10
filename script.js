c81529243939d63491b91afc5b57690c744598b2
dannyarena <arenadanny71@gmail.com>
2025-04-10 16:31:45 +0200

    add age-based discount logic to ticket price calculation

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 document.getElementById('calcolaBtn').addEventListener('click', function () {
     const km = parseInt(document.getElementById('km').value);
     const eta = parseInt(document.getElementById('eta').value);
+
+    const prezzoBase = km * 0.21;
+
+let prezzoFinale
+
+if (eta < 18) {
+    prezzoFinale = prezzoBase * 0.8;
+  } else if (eta > 65) {
+    prezzoFinale = prezzoBase * 0.6;
+  } else {
+    prezzoFinale = prezzoBase;
+  }
+
+  prezzoFinale = prezzoFinale.toFixed(2);
+  
   });
+
   
